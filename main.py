65d3adad54b1fd9698b7d858d1fde4d2c5020556
Edwin Marte Torres <kdgpro15@gmail.com>
2022-02-05 11:03:24 -0400

    Adding Requirements File

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-import time
-import unittest
+import os, requests, sys, time, unittest
+from bs4 import BeautifulSoup
+from http_request_randomizer.requests.proxy.requestProxy import RequestProxy
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException        
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 # CONSTANTS
 WAIT_TIME = 15
+WAIT_INLINE_TIME = 5
 
 # VARIABLES
 user_id = 2803902007
 
 # FUNCTIONS
-def open_website(url):
-    return driver.get(url)
+def open_website(drv, url):
+    return drv.get(url)
 
 def wait_for_element(drv, qtype, query):
     element = None
@@ -29,6 +32,7 @@ def wait_for_element(drv, qtype, query):
     return element
 
 def check_if_element_exist(drv, qtype, query):
+    time.sleep(3)
     try:
         element = drv.find_element(qtype, query)
     except NoSuchElementException:
@@ -48,6 +52,11 @@ def fill_if_exists(drv, element, content):
         return
     print("-----" + element + " not found -----")
     drv.quit()
+
+def bypass_captcha(drv, element):
+    click_if_exists(driver, el_captcha_box)
+    time.sleep(WAIT_INLINE_TIME)
+    
     
 
 # INIT
@@ -60,89 +69,35 @@ driver = webdriver.Chrome(
 )
 
 # RUNTIME
-open_website("https://pagostore.com")
+open_website(driver, "https://pagostore.com")
 el_free_fire_first_button = wait_for_element(driver, By.CSS_SELECTOR, '[alt="Recarga Free Fire"]')
 click_if_exists(driver, el_free_fire_first_button)
 el_free_fire_second_button = wait_for_element(driver, By.XPATH, '//*[contains(text(), "ID de jugador")]')
 click_if_exists(driver, el_free_fire_second_button)
 
-# exec_runtime = 1
+loop = True
 
-# while exec_runtime > 0:    
-#     el_user_id_field = wait_for_element(driver, By.XPATH, '//input[@placeholder="ID de jugador"]')
-    # fill_if_exists(driver, el_user_id_field, "")
-    # fill_if_exists(driver, el_user_id_field, user_id)
-#     el_captcha_box = check_if_element_exist(driver, By.CSS_SELECTOR, '.recaptcha-checkbox-border')
-#     el_btn_submit_freefire_userid = wait_for_element(driver, By.XPATH, '//input[@value="Ingresar"]')
+while loop:    
+    el_user_id_field = wait_for_element(driver, By.XPATH, '//input[@placeholder="ID de jugador"]')
+    el_user_id_field.clear()
+    fill_if_exists(driver, el_user_id_field, user_id)
+    time.sleep(WAIT_INLINE_TIME)
+    el_captcha_box = check_if_element_exist(driver, By.XPATH, '//*[contains(@title, "reCAPTCHA")]')
+    el_btn_submit_freefire_userid = wait_for_element(driver, By.XPATH, '//input[@value="Ingresar"]')
 
-#     if el_captcha_box:
-#         click_if_exists(driver, el_captcha_box)
-#         time.sleep(3)
-#         click_if_exists(driver, el_btn_submit_freefire_userid)
-#     else:
-#         click_if_exists(driver, el_btn_submit_freefire_userid)
+    if el_captcha_box:
+        bypass_captcha(driver, el_captcha_box)
+        time.sleep(WAIT_INLINE_TIME)
+        click_if_exists(driver, el_btn_submit_freefire_userid)
+    else:
+        click_if_exists(driver, el_btn_submit_freefire_userid)
     
-#     time.sleep(3)
-#     el_box_user_id = check_if_element_exist(driver, By.XPATH, '//*[contains(text(), "Inicia sesión con tu ID de jugador")]')
+    time.sleep(WAIT_INLINE_TIME)
+    el_box_user_id = check_if_element_exist(driver, By.XPATH, '//*[contains(text(), "Inicia sesión con tu ID de jugador")]')
     
-#     if not el_box_user_id:
-#         exec_runtime-=1
-    
-el_user_id_field = wait_for_element(driver, By.XPATH, '//input[@placeholder="ID de jugador"]')
-el_user_id_field.clear()
-fill_if_exists(driver, el_user_id_field, user_id)
-el_captcha_box = check_if_element_exist(driver, By.CLASS_NAME, 'recaptcha-checkbox-border')
-el_btn_submit_freefire_userid = wait_for_element(driver, By.XPATH, '//input[@value="Ingresar"]')
-print(el_captcha_box)
+    if not el_box_user_id:
+        loop=False
 
-if el_captcha_box:
-    click_if_exists(driver, el_captcha_box)
-    time.sleep(3)
-    click_if_exists(driver, el_btn_submit_freefire_userid)
-else:
-    click_if_exists(driver, el_btn_submit_freefire_userid)
-
-
-time.sleep(3)
-el_user_id_field = wait_for_element(driver, By.XPATH, '//input[@placeholder="ID de jugador"]')
-el_user_id_field.clear()
-fill_if_exists(driver, el_user_id_field, user_id)
-el_captcha_box = check_if_element_exist(driver, By.CLASS_NAME, 'recaptcha-checkbox-border')
-el_btn_submit_freefire_userid = wait_for_element(driver, By.XPATH, '//input[@value="Ingresar"]')
-print(el_captcha_box)
-
-if el_captcha_box:
-    print("FOUND CAPTCHA")
-    click_if_exists(driver, el_captcha_box)
-    time.sleep(3)
-    click_if_exists(driver, el_btn_submit_freefire_userid)
-else:
-    click_if_exists(driver, el_btn_submit_freefire_userid)
-
-
-time.sleep(3)
-el_user_id_field = wait_for_element(driver, By.XPATH, '//input[@placeholder="ID de jugador"]')
-el_user_id_field.clear()
-fill_if_exists(driver, el_user_id_field, user_id)
-el_captcha_box = check_if_element_exist(driver, By.CLASS_NAME, 'recaptcha-checkbox-border')
-el_btn_submit_freefire_userid = wait_for_element(driver, By.XPATH, '//input[@value="Ingresar"]')
-print(el_captcha_box)
-
-if el_captcha_box:
-    print("FOUND CAPTCHA")
-    click_if_exists(driver, el_captcha_box)
-    time.sleep(3)
-    click_if_exists(driver, el_btn_submit_freefire_userid)
-else:
-    click_if_exists(driver, el_btn_submit_freefire_userid)
-
-
-
-# el_free_fire_first_button.click()
-# el_free_fire_first_button = driver.find_element(By.CSS_SELECTOR, '[alt="Recarga Free Fire"]')
-# el_free_fire_first_button.click()
-# el_free_fire_second_button = driver.find_element(By.XPATH, '//*[contains(text(), "ID de jugador")]')
-# el_free_fire_second_button.click()
 
 time.sleep(WAIT_TIME)
 print("estamos dentro")
